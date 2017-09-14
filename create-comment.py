4e4ad3b89effbf4bda71e0879f3de9d4771074a8
heari <h.emreari@protonmail.com>
2017-09-14 11:32:41 +0300

    Everything is OK except multiline description section.

--- create-comment.py
+++ create-comment.py
@@ -18,20 +18,31 @@ class EasyComment:
     def createFile(self, fileName):
 
         f = open(self.file_name, "w+");
-        f.write("/*----------------------------------------------*")
-        f.write(" * Author       : " + self.name_author + "           *\n")
-        f.write(" * Created Date : " + self.created_date + "          *\n")
-        f.write(" * Last Update  : " + self.last_update + "           *\n")
-        f.write(" * Description  : " + self.description + "           *\n")
-        f.write(" *----------------------------------------------*")
+        f.write("/*" + '-' * 77 + "*\n")
+        f.write(" * Author       : " + self.name_author + ' ' * (80 - (19 + self.countAuthor)) + "*\n")
+        f.write(" * Created Date : " + self.created_date + ' ' * (80 - (19 + self.countDate)) + "*\n")
+        f.write(" * Last Update  : " + self.last_update + ' ' * (80 - (19 + self.countUpdate)) + "*\n")
+        f.write(" * Description  : " + self.description + ' ' * (80 - (19 + self.countDescription)) + "*\n")
+        f.write(" *" + '-' * 77 + "*\n")
         f.write(" */\n")
     
         f.close()
 
+    def wordCount(self):
+        
+        self.countAuthor = len(self.name_author)
+        self.countDate = len(self.created_date)
+        self.countUpdate = len(self.last_update)
+        self.countDescription = len(self.description)
+        
+        
 def main():
     ec = EasyComment()
+
     ec.welcoming()
     ec.getInfo()
+    ec.wordCount()
     ec.createFile(ec.file_name)
+    print("Everything is done, Your file created.\n")
 
 main()
