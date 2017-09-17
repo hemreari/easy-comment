fbe7c04ab85654dd38f0d8b07c65e9862494e9db
heari <h.emreari@protonmail.com>
2017-09-17 19:55:31 +0300

    Added other PL source file but they don't work

--- create-comment.py
+++ create-comment.py
@@ -1,22 +1,40 @@
 class EasyComment:
 
     def welcoming(self):
+
         # Shows a welcomig message to user
+        # and detects the type of the source file
+
         print("Welcome to Easy Comment");
+        print("Avaible programming languages:\n")
+        print("C [1], Python [2], Assembly [3]\n");
+        self.type_source_file = input("Your choice: ");
 
     def getInfo(self):
 
         # Gets informations about source file to write 
         # the top of the source file
         # Program works for c files, the other language will be added soon.
+
         self.file_name = input("Enter a file name: ")
         self.name_author = input("Enter author: ")
         self.created_date = input("Enter creation date: ")
         self.last_update = self.created_date
         self.description = input("Enter a description for your source file: ")
+        
+        if self.type_source_file == '1':
+            create_C_File(self, self.file_name);
+        elif self.type_source_file == '2':
+            create_Python_File(self, self.file_name);
+        elif self.type_source_file == '3':
+            create_Assem_File(self, self.file_name);
 
-    def createFile(self, fileName):
+    def create_C_File(self, fileName):
 
+        '''
+            creates a source file for C pl with comment block, if the file doesn't exist.
+            If the file is existed, writes a comment block this file.  
+        '''
         f = open(self.file_name, "w+");
         f.write("/*" + '-' * 77 + "*\n")
         f.write(" * Author       : " + self.name_author + ' ' * (80 - (19 + self.countAuthor)) + "*\n")
@@ -28,14 +46,25 @@ class EasyComment:
     
         f.close()
 
+    def create_Python_File(self, fileName):
+
+        print("This option is not avaible yet.\n")
+        self.welcoming()
+
+
+    def create_Assem_File(self, fileName):
+
+        print("This option is not avaible yet.\n")
+        self.welcoming()
+
     def wordCount(self):
         
         self.countAuthor = len(self.name_author)
         self.countDate = len(self.created_date)
         self.countUpdate = len(self.last_update)
         self.countDescription = len(self.description)
-        
-        
+
+    
 def main():
     ec = EasyComment()
 
@@ -43,6 +72,6 @@ def main():
     ec.getInfo()
     ec.wordCount()
     ec.createFile(ec.file_name)
-    print("Everything is done, Your file created.\n")
+    print("Everything is done, Your file is created.\n")
 
 main()
